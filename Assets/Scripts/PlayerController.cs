810e138694680b3c338711dd9ac06e270a9f619d
Petebro113 <50191031+Petebro113@users.noreply.github.com>
2021-03-18 13:23:06 -0700

    u

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -43,86 +44,90 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        //Flip Sprite Direction
-        if (!facingRight && moveInput > 0)
+        if (!isDead)
         {
-            Flip();
-        }
-        else if (facingRight && moveInput < 0)
-        {
-            Flip();
+            //Flip Sprite Direction
+            if (!facingRight && moveInput > 0)
+            {
+                Flip();
+            }
+            else if (facingRight && moveInput < 0)
+            {
+                Flip();
+            }
         }
     }
 
     private void Update()
     {
-        //Check if on ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (!isDead)
+        {
+            //Check if on ground
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        if (isGrounded)
-        {
-            animator.SetBool("isJumping", false);
-            extraJumps = numExtrajumps;
-            canDash = true;
-        }
-        else
-        {
-            animator.SetBool("isJumping", true);
-        }
-
-        //Horiz Movement
-        moveInput = Input.GetAxisRaw("Horizontal");
-
-        animator.SetFloat("speed", Mathf.Abs(moveInput*speed));
-        if (moveInput > 0)
-        {
-            direction = 1;
-        }
-        else if (moveInput < 0)
-        {
-            direction = -1;
-        }
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-
-        //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0 && !isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, extraJumpForce);
-            extraJumps--;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-
-        //Dashing
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing && !isGrounded)
-        {
-            if (canDash)
+            if (isGrounded)
             {
-                animator.SetBool("isDashing", true);
-                isDashing = true;
-            }
-        }
-
-        if (isDashing)
-        {
-            if (dashTime > 0)
-            {
-                rb.velocity = new Vector2(direction * dashSpeed, 0);
-                dashTime -= Time.deltaTime;
+                animator.SetBool("isJumping", false);
+                extraJumps = numExtrajumps;
+                canDash = true;
             }
             else
             {
-                animator.SetBool("isDashing", false);
-                isDashing = false;
-                canDash = false;
-                dashTime = startDashTime;
+                animator.SetBool("isJumping", true);
+            }
+
+            //Horiz Movement
+            moveInput = Input.GetAxisRaw("Horizontal");
+
+            animator.SetFloat("speed", Mathf.Abs(moveInput * speed));
+            if (moveInput > 0)
+            {
+                direction = 1;
+            }
+            else if (moveInput < 0)
+            {
+                direction = -1;
+            }
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+
+            //Jumping
+            if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0 && !isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, extraJumpForce);
+                extraJumps--;
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+
+            //Dashing
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing && !isGrounded)
+            {
+                if (canDash)
+                {
+                    animator.SetBool("isDashing", true);
+                    isDashing = true;
+                }
+            }
+
+            if (isDashing)
+            {
+                if (dashTime > 0)
+                {
+                    rb.velocity = new Vector2(direction * dashSpeed, 0);
+                    dashTime -= Time.deltaTime;
+                }
+                else
+                {
+                    animator.SetBool("isDashing", false);
+                    isDashing = false;
+                    canDash = false;
+                    dashTime = startDashTime;
+                }
             }
         }
-
-
     }
 
     void Flip()
@@ -134,7 +139,21 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void Die()
+    {
+        StartCoroutine(DieCoroutine());        
+    }
+
+    IEnumerator DieCoroutine()
+    {
+        rb.Sleep();
+        isDead = true;
+        animator.SetBool("isDead", true);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("Level 1");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         //Check if touching wall
         if (collision.gameObject.layer.ToString().Equals("Ground"))
@@ -145,12 +164,13 @@ public class PlayerController : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Spike"))
         {
-            isDead = true;
-
+            Die();
+           
         }
+
     }
 
-        void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
